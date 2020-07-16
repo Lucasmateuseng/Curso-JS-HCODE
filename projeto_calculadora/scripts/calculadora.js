295b3691f46e6e1f719072babfc5ce7c80d96ec0
Lucas <Lucas_mateuseng@outlook.com.br>
2020-07-15 23:19:21 -0300

    inicio do controller

---
+++ projeto_calculadora/scripts/calculadora.js
@@ -0,0 +1,8 @@
+//MVC - model, view, controller
+/*
+Model - Manipula os dados 
+View - Interage com o usuario
+Controller - Regra de negocio
+*/
+
+let calculadora = new CalController;
---
+++ projeto_calculadora/scripts/controller/calcController.js
@@ -0,0 +1,7 @@
+class CalController{
+    
+    constructor(){
+        
+    }
+
+}
