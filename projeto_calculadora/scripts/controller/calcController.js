b6a5b28ff0981581f3091653b8c4177c67264b60
Lucas mateus <Lucas_mateuseng@outlook.com.br>
2020-07-20 13:28:13 -0300

    20-07-2020 / 13:28

--- projeto_calculadora/scripts/controller/calcController.js
+++ projeto_calculadora/scripts/controller/calcController.js
@@ -1,21 +1,50 @@
 class CalController{
     
-    constructor(){
-        this._displayCalc = "0";
-        this._dataAtual;
+    constructor() {
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#date");
+        this._timeEl = document.querySelector("#hora");
+        //this._displayCalc = "0";
+        this._correntDate;
+        this.initialize();
     }
+
+    initialize() {      
+
+        
+    }
+
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value) {
+        return this._timeEl.innerHTML = value;
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value) {
+        return this._dateEl.innerHTML = value;
+    }
+
     get displayCalc(){
 
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
-    set displayCalc(valor){
-        this._displayCalc = valor;
+
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
     }
-    get dataAtual(){
-        return this._dataAtual;
+
+    get correntDate(){
+        return this.correntDate;
     }
-    set dataAtual(data){
-        this._dataAtual = data;
+
+    set correntDate(date){
+        this._correntDate = date;
     }
 
 }
